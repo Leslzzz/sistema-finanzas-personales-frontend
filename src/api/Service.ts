a54d23f93fc8e384fd8052fa29f432af1b812e8b
Adiel-Delgado <adieldelgado03@gmail.com>
2026-02-16 22:44:13 -0700

    Se modifico el Service.ts

--- src/api/Service.ts
+++ src/api/Service.ts
@@ -1,22 +1,21 @@
-// import api from './axios';
+import api from './axios';
 import type { LoginSchema, RegisterSchema } from '../models/Auth.Schema';
-import axios from 'axios';
 
 export const authService = {
   login: async (data: LoginSchema) => {
-    const response = await axios.post('https://sistema-finanzas-personales.up.railway.app/api/login/', data);
+    const response = await api.post('/api/login/', data);
     return response.data; 
   },
 
   register: async (data: RegisterSchema) => {
-    const response = await axios.post('https://sistema-finanzas-personales.up.railway.app/api/register/', data);
+    const response = await api.post('/api/register/', data);
     return response.data;
   },
 };
 
 export const dashboardService = {
   getSummary: async () => {
-    const response = await axios.get('https://sistema-finanzas-personales.up.railway.app/api/dashboard/home/');
+    const response = await api.get('/api/dashboard/home/');
     return response.data;
   }
 };
